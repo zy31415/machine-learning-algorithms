40fb1376a0e09ad9967778b5a9e56094afcf308e
Yang Zhang <zy31415@gmail.com>
2018-06-20 15:39:13 +0800

    Done Separating Hyperplane methods.

--- svm/lib/separating_hyperplane.py
+++ svm/lib/separating_hyperplane.py
@@ -1,36 +1,58 @@
 import numpy as np
 
 
-def separating_hyperplane(X, Y, rho):
-    '''
-    X - m*n matrix,
-        m - # of data points,
-        n - dimension of data
-    Y - m*1 vector, classification of data, be either 1 or -1
-    rho - learning rate
-    '''
-    (m, n) = X.shape
-    (_m, _n) = Y.shape
-    assert m == _m, "X and Y should have the same number of rows"
-    assert _n == 1, "Y should have only one column."
+class SeparatingHyperplane:
+    def __init__(self, rho=1., randomize_initials=False):
+        self.rho = rho
+        self.betas = []
+        self.beta0s= []
+        self.randomize_initials = randomize_initials
+        self.X = None
+        self.Y = None
 
-    # Hyperplane equation:
-    #    x.dot(beta) + beta0 = 0
-    #
-    # Initialize beta and beta0
-    beta = np.ones((n, 1))
-    beta0 = 1
+    def fit(self, X, Y):
+        '''
+        X - m*n matrix,
+            m - # of data points,
+            n - dimension of data
+        Y - m*1 vector, classification of data, be either 1 or -1
+        rho - learning rate
+        '''
+        (m, n) = X.shape
+        (_m, _n) = Y.shape
+        assert m == _m, "X and Y should have the same number of rows"
+        assert _n == 1, "Y should have only one column."
 
-    M = (X.dot(beta) + beta0) * Y
+        self.X = X
+        self.Y = Y
 
-    while (M < 0).any():
+        # Hyperplane equation:
+        #    x.dot(beta) + beta0 = 0
+        #
+        # Initialize beta and beta0
+        if self.randomize_initials:
+            # between [-1, 1]
+            beta = np.random.rand(n, 1) * 2 - 1
 
-        for i, j in np.argwhere(M < 0):
-            assert j == 0, "M matrix should have only one column"
-            xi = X[i, :].reshape(-1,1)
-            yi = Y[i, :][0]
-            beta += rho * yi * xi
-            beta0 += rho * yi
+            # between [-5, 5]
+            beta0 = np.random.uniform() * 2 - 1
+        else:
+            beta = np.ones([n, 1])
+            beta0 = 1
+
         M = (X.dot(beta) + beta0) * Y
+        self.betas = [beta.copy()]
+        self.beta0s = [beta0]
 
-    return beta, beta0
+        while (M < 0).any():
+            for i, j in np.argwhere(M < 0):
+                assert j == 0, "M matrix should have only one column"
+                xi = X[i, :].reshape(-1,1)
+                yi = Y[i, :][0]
+                beta += self.rho * yi * xi
+                beta0 += self.rho * yi
+            M = (X.dot(beta) + beta0) * Y
+            self.betas.append(beta.copy())
+            self.beta0s.append(beta0)
+
+        return beta, beta0
--- svm/test.py
+++ svm/test.py
@@ -1,17 +1,32 @@
 import numpy as np
-from separating_hyperplane import separating_hyperplane
+from separating_hyperplane import SeparatingHyperplane
+import unittest
 
 
-X = np.asarray([
-    [0, 0],
-    [1, 0],
-    [0, 1],
-    [2, 1],
-    [2, 2],
-    [1, 2]], dtype=float)
+class SeparatingHyperplaneTest(unittest.TestCase):
+    def setUp(self):
+        self.X = np.asarray([
+            [0, 0],
+            [1, 0],
+            [0, 1],
+            [2, 1],
+            [2, 2],
+            [1, 2]], dtype=float)
 
-Y = np.asarray([-1, -1, -1, 1, 1, 1], dtype=int).reshape([-1, 1])
+        self.Y = np.asarray([-1, -1, -1, 1, 1, 1], dtype=int).reshape([-1, 1])
 
-beta, beta0 = separating_hyperplane(X, Y, 1.)
+    def test_initial_value_1(self):
+        fitter = SeparatingHyperplane()
+        beta, beta0 = fitter.fit(self.X, self.Y)
+        print(beta, beta0)
 
-print(beta, beta0)
+    def test_randomized_initials(self):
+        fitter = SeparatingHyperplane(randomize_initials=True)
+
+        beta, beta0 = fitter.fit(self.X, self.Y)
+
+        print(fitter.betas, fitter.beta0s)
+
+
+if __name__ == '__main__':
+    unittest.main()
